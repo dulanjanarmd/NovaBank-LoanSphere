0609d9beb074d6b1c496a10cfa68817ca567c3f8
Dulanjna Rathnayaka <dulanjanarmd@gmail.com>
2026-07-21 00:05:39 +0530

    feat(springboot): add main application entry point

---
+++ springboot/src/main/java/com/novabank/loansphere/LoansphereApplication.java
@@ -0,0 +1,11 @@
+package com.novabank.loansphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansphereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoansphereApplication.class, args);
+    }
+}
