857e8c55980a4fc532df2221153271ce22f1e3b3
Dulanjna Rathnayaka <dulanjanarmd@gmail.com>
2026-07-29 22:46:10 +0530

    feat: initialize React root with BrowserRouter in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './contexts/ThemeContext.jsx'
-import { LangProvider } from './contexts/LangContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <LangProvider>
-          <App />
-        </LangProvider>
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
